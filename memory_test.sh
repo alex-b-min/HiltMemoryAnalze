868947e3a60df209175f7c2d8c4fda9535146ee1
alex-b-min <alex.b.min@selvas.com>
2024-05-28 18:08:11 +0900

    feat: 지연 시간 수정

--- memory_test.sh
+++ memory_test.sh
@@ -6,14 +6,19 @@ adb shell dumpsys meminfo > before_memory.txt
 # 약간의 지연 시간
 sleep 1
 
-# 앱 실행
-adb shell am start -n com.example.hiltmemoryanalyze/.MainActivity
+# 앱 실행 [ com.example.hilt(싱글턴 테스트 모듈) / com.example.hilt_componenttree(상호참조 트리 테스트 모듈) / ]
+# 원하는 모듈로 실행 시키기 위해서 위의 패키지 네임을 넣고 실행을 시켜야 함
+adb shell am start -n com.example.hilt/.MainActivity
 
 # 약간의 지연 시간 후에 메모리 상태 확인
-sleep 1
+sleep 0.01
 
 # 앱을 실행한 후 메모리 상태 확인
 adb shell dumpsys meminfo > after_memory.txt
 
 echo "메모리 상태 비교 결과:"
 diff before_memory.txt after_memory.txt
+
+# 앱 종료 [ com.example.hilt(싱글턴 테스트 모듈) / com.example.hilt_componenttree(상호참조 트리 테스트 모듈) / ]
+# 원하는 모듈로 실행 시키기 위해서 위의 패키지 네임을 넣고 실행을 시켜야 함
+adb shell am force-stop com.example.hilt
